9effbf460afbd3a27ae0a626229e0ecdd826ea0c
Hata_Naoki <hata@bitriver.jp>
2022-06-14 09:41:04 +0900

    ⚗️ 不要な記述でいつか消すが、クリッとイベントを作成してみた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,25 @@ import styles from "../styles/Home.module.css";
 import { Main } from "../components/Main";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
+  const handleClick = useCallback((e) => {
+    console.log("1");
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a
+        // href="/about"
+        onClick={handleClick}
+      >
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </div>
