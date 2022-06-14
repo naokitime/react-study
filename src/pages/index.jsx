a2428af469f42690745809e8a3c95b8c58e08c1e
Hata_Naoki <hata@bitriver.jp>
2022-06-14 16:25:01 +0900

    ✨ APIを叩いて表示した

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,40 +1,32 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Main } from "../components/Main";
-import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
+import { useCallback, useEffect, useState } from "react";
 
 const Home = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    setPosts(json);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-
-      <div className={styles.body}>
-        {props.isShow ? <h1>{props.count}</h1> : null}
-        <button className={styles.button} onClick={props.hadleClick}>
-          ボタン
-        </button>
-
-        <button className={styles.button} onClick={props.handleDisplay}>
-          {props.isShow ? "表示" : "非表示"}
-        </button>
-
-        <input type="text" value={props.text} onChange={props.handleChange} />
-        <button className={styles.button} onClick={props.handleAdd}>
-          追加
-        </button>
-        <ul>
-          {props.array.map((item) => {
-            return <li key={item}>{item}</li>;
-          })}
-        </ul>
-      </div>
-
-      <Main page="index" />
-      <Footer />
+      <ol>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>;
+        })}
+      </ol>
     </div>
   );
 };
