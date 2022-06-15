43784c4205cd7bf6d52ac0ad622e55928f83f5b0
Hata_Naoki <hata@bitriver.jp>
2022-06-15 17:08:03 +0900

    🚚 複数形になっていた英語を単数に変更した

--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -14,22 +14,25 @@ const useFetchArray = (url) => {
 
 const API_URL = "https://jsonplaceholder.typicode.com";
 
+//posts
+
 export const usePosts = () => {
   return useFetchArray(`${API_URL}/posts`);
 };
-
-export const useComments = () => {
-  return useFetchArray(`${API_URL}/posts`);
+export const usePostsByUserId = (id) => {
+  return useFetchArray(id ? `${API_URL}/posts?userId=${id}` : null);
 };
 
+// user
 export const useUsers = () => {
   return useFetchArray(`${API_URL}/users`);
 };
 
-export const useCommentsByPostsId = (id) => {
-  return useFetchArray(id ? `${API_URL}/comments?postId=${id}` : null);
+//comment
+export const useComments = () => {
+  return useFetchArray(`${API_URL}/posts`);
 };
 
-export const usePostsByUserId = (id) => {
-  return useFetchArray(id ? `${API_URL}/posts?userId=${id}` : null);
+export const useCommentsByPostId = (id) => {
+  return useFetchArray(id ? `${API_URL}/comments?postId=${id}` : null);
 };
