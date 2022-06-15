454a2f63450e0eb60350ccfcaad8ad9f2f5d1fda
Hata_Naoki <hata@bitriver.jp>
2022-06-15 13:09:38 +0900

    🔥 Indexから不要なコードを削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,39 +1,17 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Main } from "../components/Main";
-import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 
-const About = (props) => {
+const Index = () => {
   return (
     <div className={styles.container}>
       <Head>
         <title>Abut Page</title>
       </Head>
       <Header />
-
-      <div className={styles.body}>
-        {props.isShow ? <h1>{props.doubleCount}</h1> : null}
-        <button className={styles.button} onClick={props.hadleClick}>
-          ボタン
-        </button>
-        <button className={styles.button} onClick={props.handleDisplay}>
-          {props.isShow ? "表示" : "非表示"}
-        </button>
-
-        <input type="text" value={props.text} onChange={props.handleChange} />
-        <button className={styles.button} onClick={props.handleAdd}>
-          追加
-        </button>
-        <ul>
-          {props.array.map((item) => {
-            return <li key={item}>{item}</li>;
-          })}
-        </ul>
-      </div>
-      <Main page="about" />
-      <Footer />
+      <h1>Next.jsで学ぶReact講座</h1>
+      <p>JSONPlaecholderのAPIを色々叩いてみるよ！</p>
     </div>
   );
 };
-export default About;
+export default Index;
