49ec6f62174c0e7aba26b3fd3483f29d3dd30de8
Hata_Naoki <hata@bitriver.jp>
2022-06-15 11:56:41 +0900

    ✨ ユーザーのfetch、fetcherの共通化

--- src/pages/post/[id].jsx
+++ src/pages/post/[id].jsx
@@ -1,19 +1,15 @@
 import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 import { Header } from "../../components/Header";
-import { useRouter } from "next/router";
+import { usePost } from "src/hooks/usePost";
+import { Post } from "src/components/Post";
 
 const PostId = () => {
-  const router = useRouter();
-  console.log(router.query.id);
 
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Index Page</title>
-      </Head>
       <Header />
-      <div>{router.query.id}</div>
+      <Post/>
     </div>
   );
 };
