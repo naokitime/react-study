bcc8b3efcaa49ecfefc5bc4d83656206b47a33a6
Hata_Naoki <hata@bitriver.jp>
2022-06-15 17:55:31 +0900

    💄 Tailwind CSSを使って既存の書き換え

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-import classes from "./Header.module.css";
-
 const NAV_ITEMS = [
   { href: "/", label: "Index" },
   { href: "/posts", label: "Posts" },
@@ -11,11 +9,13 @@ const NAV_ITEMS = [
 export const Header = () => {
   return (
     <div>
-      <header className={classes.header}>
+      <header className="flex justify-center items-center border-b w-full h-24">
         {NAV_ITEMS.map((item) => {
           return (
             <Link key={item.href} href={item.href}>
-              <a className={classes.anchor}>{item.label}</a>
+              <a className="inline-block py-2 px-6 text-xl hover:text-blue-500 hover: focus:text-blue-500 active:text-blue-500">
+                {item.label}
+              </a>
             </Link>
           );
         })}
