a342c95948335c612a2ef961f49407415f02a59b
Hata_Naoki <hata@bitriver.jp>
2022-06-16 10:56:25 +0900

    ✨ ユーザー詳細ページでSSRを実践

--- src/pages/users/[id].jsx
+++ src/pages/users/[id].jsx
@@ -1,12 +1,29 @@
 import { Header } from "../../components/Header";
 import { UserComponet } from "src/components/User";
+import { SWRConfig } from "swr";
 
-const UserId = () => {
+export const getServerSideProps = async (ctx) => {
+  const { id } = ctx.query;
+  const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+  const user = await fetch(API_URL);
+  const userData = await user.json();
+
+  return {
+    props: {
+      fallback: {
+        [API_URL]: userData,
+      },
+    },
+  };
+};
+
+const UserId = (props) => {
+  const { fallback } = props;
   return (
-    <div>
+    <SWRConfig value={{ fallback }}>
       <Header />
       <UserComponet />
-    </div>
+    </SWRConfig>
   );
 };
 
